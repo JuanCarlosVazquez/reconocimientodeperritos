d11cb4c1393c8e89d70bc963c64c68c8b6ef4842
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-09-17 23:25:25 -0700

    Updates

---
+++ precache-manifest.254284e5d4834ec06344e30937fc918b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64110282f016024488a299fa07a2127",
+    "url": "/reconocimientodeperritos/index.html"
+  },
+  {
+    "revision": "d427c4a6a6af3891a65e",
+    "url": "/reconocimientodeperritos/static/css/main.48b53571.chunk.css"
+  },
+  {
+    "revision": "fa856bf631e9d668eba2",
+    "url": "/reconocimientodeperritos/static/js/2.3b4e8fe3.chunk.js"
+  },
+  {
+    "revision": "561ba586423fc8b50fb173a3cde1dd2e",
+    "url": "/reconocimientodeperritos/static/js/2.3b4e8fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d427c4a6a6af3891a65e",
+    "url": "/reconocimientodeperritos/static/js/main.22595cab.chunk.js"
+  },
+  {
+    "revision": "89cc8ea3a7e5262994ba",
+    "url": "/reconocimientodeperritos/static/js/runtime-main.8562b609.js"
+  }
+]);
